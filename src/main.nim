85407709c6f915c99ffba37fbd9f2da785e901ba
Skyforce787 <arnaud.bruyer@gmail.com>
2015-10-06 10:27:49 +0000

    utils

--- src/main.nim
+++ src/main.nim
@@ -1,3 +1,9 @@
-include sub
+include utils
+
 echo "Ne mets pas tes doigts dans la porte !"
 echo "Tu risque de te faire pincer très fort !"
+
+var x: int
+x = 10
+x+=1
+echo x
---
+++ src/utils.nim
@@ -0,0 +1,5 @@
+template `+=` (a, b: expr): expr =
+  a = a+b
+
+template `-=` (a, b: expr): expr =
+  a = a-b
