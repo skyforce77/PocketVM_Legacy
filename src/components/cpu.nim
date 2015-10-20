44def1fe5467072a68979710d6d8338ee9fc101f
Skyforce77 <arnaud.bruyer@gmail.com>
2015-10-20 21:37:47 +0200

    support des short/int/long

--- src/components/cpu.nim
+++ src/components/cpu.nim
@@ -1,8 +1,8 @@
-import os, strutils, memory, cpu.instructions, cpu.types
+import os, strutils, unsigned, memory, cpu.instructions, cpu.types
 
 #CPU Object
 type CPU = object
-  registers: array[16, uint32]
+  registers: array[16, uint64]
   cursor: int
 
 #Loading CPU
@@ -25,12 +25,44 @@ proc readChar(this: var CPU): char =
   this.cursor+=1
   return typ
 
+proc readNBytes(this: var CPU, n: int): uint64 =
+  var value: uint64 = 0
+  for i in countdown(n-1,0):
+    var nvalue: uint64 = this.readByte()
+    nvalue = nvalue shl uint64(8*i)
+    value = value+nvalue
+  return value
+
+proc readShort(this: var CPU): uint16 =
+  return uint16(this.readNBytes(2))
+
+proc readInt(this: var CPU): uint32 =
+  return uint32(this.readNBytes(4))
+
+proc readLong(this: var CPU): uint64 =
+  return this.readNBytes(8)
+
 proc readString(this: var CPU): string =
   var str: string = ""
   while bufferRead(this.cursor) != char(0):
     str = str&bufferRead(this.cursor)
     this.cursor+=1
+  this.cursor+=1
   return str
+
+proc readValueForRegister(this: var CPU): uint64 =
+  case this.readByte():
+  of TYPE_REGISTER:
+    return this.registers[int(this.readByte())]
+  of TYPE_BYTE:
+    return this.readByte()
+  of TYPE_SHORT:
+    return this.readShort()
+  of TYPE_INT:
+    return this.readInt()
+  of TYPE_LONG:
+    return this.readLong()
+  else: return uint(0)
 
 #Instructions
 proc execPrint(this: var CPU) =
@@ -41,26 +73,28 @@ proc execPrint(this: var CPU) =
     write(stdout, this.readChar())
   of TYPE_REGISTER:
     write(stdout, this.registers[int(this.readChar())])
-  else: nil
+  else: discard
 
 proc execBeep(this: var CPU) = #todo
   case this.readByte():
   of TYPE_BYTE:
     write(stdout, this.readString())
-  else: nil
-  
-proc execMove(this: var CPU) =
-  var toMove: uint32 = 0
-  case this.readByte():
-  of TYPE_BYTE:
-    toMove = uint32(this.readByte())
-  of TYPE_REGISTER:
-    toMove = uint32(this.registers[int(this.readByte())])
-  else: nil
+  else: discard
+
+proc execMove(this: var CPU) = #need to support int and long
+  var toMove: uint64 = this.readValueForRegister()
   case this.readByte():
   of TYPE_REGISTER:
     this.registers[int(this.readByte())] = toMove
-  else: nil
+  else: discard
+
+proc execAdd(this: var CPU) = #need to support int and long
+  let arg1: uint64 = this.readValueForRegister()
+  let arg2: uint64 = this.readValueForRegister()
+  case this.readByte():
+  of TYPE_REGISTER:
+    this.registers[int(this.readByte())] = arg1+arg2
+  else: discard
 
 #Instructions slector
 proc exec(this: var CPU) =
@@ -71,8 +105,9 @@ proc exec(this: var CPU) =
       this.execBeep()
     of INSTRUCTION_MOVE:
       this.execMove()
-    else: nil
-  this.cursor+=1
+    of INSTRUCTION_ADD:
+      this.execAdd()
+    else: discard
 
 #Start execution
 proc start(this: var CPU) =
--- src/components/cpu/types.nim
+++ src/components/cpu/types.nim
@@ -2,5 +2,6 @@ const
   TYPE_REGISTER*: uint8 = 0x01
   TYPE_STRING*: uint8 = 0x02
   TYPE_BYTE*: uint8 = 0x03
-  TYPE_INT*: uint8 = 0x04
-  TYPE_LONG*: uint8 = 0x05
+  TYPE_SHORT*: uint8 = 0x04
+  TYPE_INT*: uint8 = 0x05
+  TYPE_LONG*: uint8 = 0x06
