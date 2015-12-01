843b93f9b24e2fb181ac78870e4722e06297c3f0
MATHIEU LEMEE <lemee@salle222-14.arda>
2015-12-01 09:00:18 +0000

    ajout de la pile

--- src/components/memory.nim
+++ src/components/memory.nim
@@ -1,13 +1,23 @@
 import os, strutils
 
 const BUFFER_SIZE = 4096
-var buffer: array[BUFFER_SIZE, char]
-var size: int64 = 0
+type Memory = object
+    buffer: array[BUFFER_SIZE, char]
+    size: int64
+    stack: seq[uint64]
 
-proc bufferSize*(): int64 = size
-proc bufferRead*(index: int64): char = buffer[index]
+proc bufferSize*(this: var Memory): int64 = this.size
+proc bufferRead*(this: var Memory, index: int64): char = this.buffer[index]
 
-proc toMemory*(file: File) =
-  while not file.endOfFile() and size < BUFFER_SIZE:
-    buffer[size] = file.readChar()
-    size+=1
+proc loadCode*(this: var Memory, file: File) =
+  while not file.endOfFile() and this.size < BUFFER_SIZE:
+    this.buffer[this.size] = file.readChar()
+    this.size+=1
+
+proc push*(this: var Memory, value: uint64) =
+    this.stack.add(value)
+
+proc pop*(this: var Memory, value: uint64): uint64 =
+    var last = this.stack[this.stack.len()]
+    this.stack.delete(this.stack.len())
+    return last
